4512efce04f444365361c4cb515796aa57b8ab74
Anther <anthrzy@gmail.com>
2013-03-05 10:27:51 -0500

    Fixed bug where Query.php did not generate a an "IS NULL" query correctly

--- test/ScopeTest.php
+++ test/ScopeTest.php
@@ -52,8 +52,7 @@ class ScopeTest extends DatabaseTest
 	{
 		parent::tear_down();
 	}
-	
-	
+
 	public function test_is_tito_scope()
 	{
 		$tito = IsNotBob::scoped()->is_tito()->all();
@@ -180,5 +179,12 @@ class ScopeTest extends DatabaseTest
 		$this->assertEquals(1,count($tito->parents));
 		$this->assertEquals(0,count($bush->parents));
 	}
+	
+	public function test_query_with_null_condition_is_generated_correctly_through_scope()
+	{
+		$result = IsNotBob::all(array('conditions'=>array('updated_at'=>null)));
+		$this->assertEquals(3,count($result));
+	}
+	
 }
 ?>
